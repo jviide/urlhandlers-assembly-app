3bfa70e8aaea0963c05c09732654713012f010a6
Joachim Viide <jviide@iki.fi>
2016-08-02 11:33:31 +0300

    Add visual scoring

--- static/js/scores.js
+++ static/js/scores.js
@@ -1,13 +1,4 @@
 function main(parentElement, teams, apiPath) {
-    var elements = {};
-
-    teams.forEach(function(team) {
-        var element = document.createElement("div");
-        element.className = "team " + team;
-        elements[team] = element;
-        parentElement.appendChild(element);
-    });
-
     function get(obj, key, _default) {
         if (!obj || !obj.hasOwnProperty(key)) {
             return _default;
@@ -16,11 +7,27 @@ function main(parentElement, teams, apiPath) {
     }
 
     function updateScores(scores) {
+        var max = 0;
         teams.forEach(function(team) {
             var teamScore = get(scores, team, {});
             var userAgents = get(teamScore, "user_agents", 0);
             var remoteAddrs = get(teamScore, "remote_addrs", 0);
-            elements[team].textContent = "Team " + team + ": " + userAgents + " user agents, " + remoteAddrs + " addresses";
+            max = Math.max(max, userAgents + remoteAddrs);
+        });
+        max += 5;
+
+        teams.forEach(function(team) {
+            var element = document.getElementById(team);
+            if (!element) {
+                return;
+            }
+
+            var teamScore = get(scores, team, {});
+            var userAgents = get(teamScore, "user_agents", 0);
+            var remoteAddrs = get(teamScore, "remote_addrs", 0);
+            var score = userAgents + remoteAddrs;
+            var percentage = max > 0 ? 100 * score / max : 0;
+            element.style.height = Math.round(percentage) + "%";
         });
     }
 
